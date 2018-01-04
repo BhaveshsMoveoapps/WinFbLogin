9fa1d0c78c5018aae7bd658f51c4a2e41b86cc7c
Bhavesh <bhaveshs@moveoapps.in>
2018-01-04 14:12:48 +0530

    index file updated

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import {
   NativeModules,
-  Platform
 } from 'react-native';
 
 class RNWinFbLogin {
- 
   static startLogin(){
-    if (Platform.OS === "ios" || Platform.OS === "android") {
       return new Promise((resolve, reject) => {
-        NativeModules.RNWinFbLogin.login((e) => {
-          return reject({ error: e });
-        },(e) => {
-          return resolve(e);
-        });
-      });
-    } else {
       NativeModules.RNWinFbLogin.login((e) => {
         return reject({ error: e });
       },(e) => {
         return resolve(e);
       });
-    }
+    });
   }
 }
 
